c45298eae07dd89985580261ce29f81156c273c0
Nathan Chan <123600671+nathanc726@users.noreply.github.com>
2023-12-04 17:49:42 -0700

    Delete app directory

--- app/PageContext.js
+++
@@ -1,9 +0,0 @@
-// app/PageContext.js
-import React from 'react';
-
-const PageContext = React.createContext({
-  showMainPage: true,
-  setShowMainPage: () => {}
-});
-
-export default PageContext;
